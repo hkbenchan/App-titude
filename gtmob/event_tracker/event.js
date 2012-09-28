b6349ccbb892095c53f51d7899a2e184bec9a90f
google.johnh8888 <google.johnh8888@gmail.com>
2012-09-28 01:49:44 -0400

    Fixed error.

--- gtmob/event_tracker/event.js
+++ gtmob/event_tracker/event.js
@@ -18,7 +18,7 @@ $(function() {
 				console.log(data);
 				var i = 0;
 				var output = "";
-				$.each(data.date,function(key,val) {
+				$.each(data[0],function(key,val) {
 					output += '<div class="event_collapsible" data-role="collapsible" data-theme="b" data-content-theme="c">';
 					output += '<h3>' + val + '</h3>';
 					output += '<ul id="events_list" data-role="listview" data-filter="true" data-theme="c">';
