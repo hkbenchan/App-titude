7abcbacb7d688b0d66b8578ef643f660baed7719
google.johnh8888 <google.johnh8888@gmail.com>
2012-09-28 00:32:45 -0400

    Experimenting.

--- gtmob/event_tracker/event.js
+++ gtmob/event_tracker/event.js
@@ -19,7 +19,7 @@ $(function() {
 	        	var output = '<ul data-role="listview" data-filter="true">';
 				$.each(data,function(key,val) {
 					output += '<li>';
-					output += '<h3>' + val.Title + '</h3>';
+					output += '<h3>' + val[0].Title + '</h3>';
 					output += '</li>';
 				});
 				output += '</ul>';
