bb989fae4fc942c649d58eb34fb7c76c88cb5d3c
Eduardo Perez <edrdo.prz@gmail.com>
2012-09-29 15:57:19 -0400

    fixed colon

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -65,11 +65,7 @@ $(function() {
 				var hour = data.StartTime.split(" ")[1];
 				
 				$('#actual_event').remove();
-				$('#actual_event').append('<div data-role="content" data-theme="b" data-content-theme="c"><p>Name: ' + data.Title + '</br></br>
-																										  Contact: ' + data.Email_address + '</br></br>
-																										  Location: ' + data.Name + '</br></br>
-																										  Time: ' + hour + '</br></br>
-																										  Description: ' + data.Description + '</p></div>');
+				$('#actual_event').append('<div data-role="content" data-theme="b" data-content-theme="c"><p>Name: ' + data.Title + '</br></br>Contact: ' + data.Email_address + '</br></br>Location: ' + data.Name + '</br></br>Time: ' + hour + '</br></br>Description: ' + data.Description + '</p></div>');
 				
 				});
 	        },
