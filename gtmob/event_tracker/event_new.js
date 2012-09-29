aca357f045c09824132c369b052830c9eac04959
Eduardo Perez <edrdo.prz@gmail.com>
2012-09-29 12:26:36 -0400

    changes to UI

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -1,40 +1,157 @@
-$(function() {
- 		// Handler for .ready() called.
-        console.log('ready');
- 
-        //Bind to the create so the page gets updated with the listing
-        $('#lists_events_page').bind('pagebeforeshow',function(event, ui){
-                console.log('pagebeforeshow');
-        
-                //Remove the old rows
-                $( ".event_list_row" ).remove();
-                
-                //JQuery Fetch The New Ones
-                $.ajax({
-                        url: "api/event",
-                        dataType: "json",
-                async: false,
-                success: function(data, textStatus, jqXHR) {
-                                console.log(data);
-                        //Create The New Rows From Template
-                        $( "#event_list_row_template" ).tmpl( data ).appendTo( "#event_list" );
-                },
-                error: ajaxError
-                });
-                
-                $('#event_list').listview('refresh');
-        });
+var current_page; // indicator for the page view
+var pageViewLimit = 20;
 
- 
+$(function() {
+ // Handler for .ready() called.
+	console.log('ready');
+
+	//Bind to the create so the page gets updated with the listing
+	$('#browse_all_events_page').bind('pagebeforeshow',function(event, ui){
+		console.log('pagebeforeshow');
+		
+		//JQuery Events
+		$.ajax({
+			url: "api/event",
+			dataType: "json",
+	        async: false,
+	        success: function(data, textStatus, jqXHR) {
+				console.log(data);
+				var i = 0;
+				var output = '';
+				$.each(data.date,function(key,val) {
+					output += '<div id="event_collapse" class="event_collapsible" data-role="collapsible" data-collapsed="false" data-theme="a">';
+					output += '<h3>' + val + '</h3>';
+					output += '<div>'
+					output += '<ul class="event_list" id="event_list" data-role="listview" data-inset="true" data-filter="true" data-theme="c" data-divider-theme="b">';
+					$.each(data[i],function(key,val) {
+						output += '<li class="event_list_row" id="event_' + val.ID + '">';
+						output += '<a href="#" data-transition="slide">'
+						output += '<h3>' + val.Title + '</h3>';
+						output += '</a>'
+						output += '</li>';
+					});
+					output += '</ul>';
+					output += '</div>';
+					output += '</div>';
+					i++;
+				});
+				$('#post_all_events').html(output);
+	        },
+	        error: ajaxError
+		});
+		
+		$('.event_list').listview();
+		$('.event_collapsible').collapsible();
+	});
+});
+
+function formatList(ele_div,data) {
+	var l = data['date'].length;
+
+	for (i=0;i<l;i++){
+		// day -- a button display the day
+		var js_obj = { date : data['date'][i] };
+		$('#dateHeaderTemplate').tmpl(js_obj).appendTo(ele_div);
+		var l2 = data[i].length;
+		for (j=0; j<l2; j++){
+			$('#menuiconTemplate').tmpl(data[i][j]).appendTo(ele_div);
+		}
+	}
+}
+
+function eventIndex(){
+	$.ajax({
+		url: "api/event",
+		context: document.body,
+		success: function(data){
+			formatList($('#IndexResult').empty(),eval('('+data+')'));
+		}
+	});
+}
+
+function eventGet(id){
+	$.ajax({
+		url: "api/event/"+id,
+		context: document.body,
+		success: function(data){
+			$('#GetResult').empty().html(data);
+		}
+	});
+}
+
+function eventPagination(limit,offset){
+	$.ajax({
+		url: "api/event/"+offset+"/page/"+limit,
+		context: document.body,
+		success: function(data){
+			formatList($('#IndexResult').empty(),eval('('+data+')'));
+		}
+	});
+}
+
+/*
+function simplePost(){
+	$.ajax({
+		url: "api/simple",
+		data: {'itemValue': 'testItemValue'},
+		context: document.body,
+		type: 'POST',
+		success: function(data){
+			$('#PostResult').html(data);
+		}
+	});
+}
+function simplePut(){
+	$.ajax({
+		url: "api/simple/testItemValue",
+		context: document.body,
+		data: {'itemValue': 'testItemNewValue'},
+		headers: {'X-HTTP-Method-Override': 'PUT'},
+		type: 'POST',
+		success: function(data){
+			$('#PutResult').html(data);
+		}
+	});
+}
+function simpleDelete(){
+	$.ajax({
+		url: "api/simple/testItem",
+		context: document.body,
+		type: 'DELETE',
+		success: function(data){
+			$('#DeleteResult').html(data);
+		}
+	});
+}
+*/
+
+function prevPageView(){
+	if (current_page > 1) {
+		// go back to previous pageViewLimit result
+	}
+}
+
+function nextPageView(){
+	
+}
+
+/* Do it once the page finish loading */
+
+$(document).ready(function(){
+	// start with first pageViewLimit most recent events
+	eventPagination(pageViewLimit,0);
+	current_page = 1;
+});
+
 /******************************************************************************/
- 
+
 function ajaxError(jqXHR, textStatus, errorThrown){
-        console.log('ajaxError '+textStatus+' '+errorThrown);
-        $('#error_message').remove();
-        $("#error_message_template").tmpl( {errorName: textStatus, errorDescription: errorThrown} ).appendTo( "#error_dialog_content" );
-        $.mobile.changePage($('#error_dialog'), {
-                transition: "pop",
-                reverse: false,
-                changeHash: false
-        });
+	console.log('ajaxError '+textStatus+' '+errorThrown);
+	$('#error_message').remove();
+	$("#error_message_template").tmpl( {errorName: textStatus, errorDescription: errorThrown} ).appendTo( "#error_dialog_content" );
+	$.mobile.changePage($('#error_dialog'), {
+		transition: "pop",
+		reverse: false,
+		changeHash: false
+	});
 }
