02f9c79e236293f5cbd18cee2275ea4f3a33d927
google.johnh8888 <google.johnh8888@gmail.com>
2012-09-29 13:22:42 -0400

    Fixed error.

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -24,7 +24,7 @@ $(function() {
 					/*output += '<div id="event_collapse" class="event_collapsible" data-role="collapsible" data-collapsed="false" data-theme="a">';
 					output += '<ul class="event_list" id="event_list" data-role="listview" data-inset="true" data-filter="true" data-theme="c" data-divider-theme="b">';*/
 					$.each(data[i],function(key,val) {
-						$('#event_list' + i).append('<li><a href="#" data-transition="slide"><h3>' + val.Title + '</h3></a></li>';
+						$('#event_list' + i).append('<li><a href="#" data-transition="slide"><h3>' + val.Title + '</h3></a></li>');
 						/*output += '<li class="event_list_row" id="event_' + val.ID + '">';
 						output += '<a href="#" data-transition="slide">'
 						output += '<h3>' + val.Title + '</h3>';
