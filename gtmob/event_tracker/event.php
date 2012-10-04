5ebf9e31707b50ead958c8d816b197085f65e31d
Ben Chan <ustbenchan@gmail.com>
2012-10-04 19:29:26 -0400

    added update function not tested

--- gtmob/event_tracker/event.php
+++ gtmob/event_tracker/event.php
@@ -21,6 +21,35 @@ function preprocessDataCheck($search_str, $error_msg) {
 }
 
 /**
+* A helper function that checks the permission of the person (given event_id)
+*/
+function eventPermissionRight($event_id) {
+	if (!is_numeric($event_id)) {
+		return false;
+	}
+	
+	global $_USER;
+	$acctName = $_USER['uid'];
+	
+	$dbQuery = sprintf("SELECT a1.* from `AuthUser` a1, 
+	(SELECT a2.OnBehalf FROM `AuthUser` a2
+	JOIN `Organization` o ON a2.OnBehalf = o.ID
+	JOIN `CreatorOwn` c ON a2.ID = c.AuthUserID
+	JOIN `Event` e on c.CreatorID = e.CreatorID
+	WHERE e.ID = '%s') as b
+	WHERE a1.AcctName = '%s'
+	AND a1.OnBehalf = b.OnBehalf",
+	mysql_real_escape_string($event_id),mysql_real_escape_string($acctName));
+	
+	$result = getDBResultNoHarm($dbQuery);
+	if (count($result)>0) {
+		return true;
+	} else {
+		return false;
+	}
+	
+}
+/**
 * A helper function that group the events by date
 */
 function groupByStartDate($input_array = null) {
@@ -334,18 +363,275 @@ function postEvent($event = null) {
         echo json_encode($result);
 }
  
-function updateEvent($id,$event) {
-        /*$dbQuery = sprintf("UPDATE comments SET comment = '%s' WHERE id = '%s'",
-                mysql_real_escape_string($comment),
-                mysql_real_escape_string($id));
-        
-        $result = getDBResultAffected($dbQuery);
-        
-        header("Content-type: application/json");
-        echo json_encode($result);*/
+function updateEvent($event_id) {
+
+	// verify the permission to edit the event
+	if (!eventPermissionRight($event_id)) {
+		$GLOBALS["_PLATFORM"]->sandboxHeader("HTTP/1.1 404 Not Found");
+		header("Content-type: application/json");
+        echo json_encode(array('error_msg'=> 'You do not have the permission.'));
+		die();
+	}
+	
+	/**
+	* update split into 5 parts
+	* (1) Creator Table
+	* (2) CreatorOwn Table
+	* (3) Location Table
+	* (4) EventType Table
+	* (5) Event Table
+	*/
+	
+	/**
+	* Flow:
+	* Check if the user has the right to update event (Query if the username exist on AuthUser)
+	* If true, then proceed; else die()
+	* Get event contact (email, phone, additional contact)
+	* (1) Insert to the Creator table, get back the CreatorID
+	* (2) Use the CreatorID and account name to insert to CreatorOwn Table
+	* (3) Get the LatCoord, LongCoord, Name and insert to Location, get back the LocationID
+	* Get the EventTypeDesc and search if it exists in the table
+	* If true, use that EventTypeID; else (4) insert into EventType Table and get back the EventTypeID
+	* (5) Use all data that needed to update the Event table, get back the EventID and return success or not
+	*/
+	
+	/*
+	* Data needed:
+	* OrganizationName
+	* Email, Phone, Contact
+	* LatCoord, LongCoord, Location
+	* EventTypeDesc
+	* Title, Description, StartTime, EndTime
+	*/
+	
+	// check the permission
+	global $_USER;
+	$acctName = $_USER['uid'];
+	
+	// pre-check OrganizationName before do permission check
+	
+	preprocessDataCheck("OrganizationName","Organization Name cannot be empty");
+			
+	s_echo("Just started");
+	$dbQuery = sprintf("SELECT `AuthUser`.*, o.OrganizationName FROM `AuthUser`
+	JOIN `Organization` o ON `AuthUser`.OnBehalf = o.ID
+	WHERE `AuthUser`.AcctName = '%s' AND o.OrganizationName = '%s'",
+	mysql_real_escape_string($acctName), mysql_real_escape_string($_POST['OrganizationName']));
+	
+	s_echo("Permission SQL: " . $dbQuery);
+	
+	$permission = getDBResultRecord($dbQuery); // the server will terminate if no permission
+	
+	s_echo("You have the permission.");
+	
+	// do all the necessary checking in here
+	// fields that cannot empty: Location, Title, StartTime, EndTime, EventTypeDesc
+	
+	preprocessDataCheck("Location","Location cannot be empty");
+	preprocessDataCheck("Title","Title cannot be empty");
+	preprocessDataCheck("StartTime","Start Time cannot be empty");
+	preprocessDataCheck("EndTime","End Time cannot be empty");
+	preprocessDataCheck("EventTypeDesc","Event Type Name cannot be empty");
+	
+	// StartTime <= EndTime
+	$date_s = strtotime($_POST['StartTime']);
+	$date_e = strtotime($_POST['EndTime']);
+	if ($date_s > $date_e)
+	{
+		$GLOBALS["_PLATFORM"]->sandboxHeader("HTTP/1.1 500 Internal Server Error");
+		header("Content-type: application/json");
+        echo json_encode(array('error_msg'=> "Start Time should be earlier than End Time"));
+		die();
+	}
+	
+	// Insert to the Creator Table
+	$dbQuery = sprintf("INSERT INTO Creator (Email_address,Phone_number,Contact) VALUES ('%s',%d,'%s')",
+				mysql_real_escape_string($_POST[ 'Email' ]),(int)mysql_real_escape_string($_POST[ 'Phone' ]),
+				mysql_real_escape_string($_POST[ 'Contact' ]));
+	
+	s_echo("Creator SQL: ".$dbQuery);
+	
+	$result = getDBResultInserted($dbQuery,'ID'); // get back the CreatorID
+	
+	s_echo("Creator Table inserted");
+	
+	$CreatorID = $result['ID'];
+	
+	s_echo("CreatorID: ".$CreatorID);
+	
+	// Insert to the CreatorOwn Table
+	$dbQuery = sprintf("INSERT INTO CreatorOwn (CreatorID,AuthUserID) VALUES ('%s','%s')",
+				mysql_real_escape_string($CreatorID),mysql_real_escape_string($permission['ID']));
+	
+	s_echo("CreatorOwn SQL: ".$dbQuery);
+	
+	getDBResultInserted($dbQuery,'dummyID');
+	
+	s_echo("CreatorOwn Table inserted");
+	
+	// Search if Location has the same record
+	$LocationCall = 0; // Avoid checking again if we need to insert it later
+	if ((is_float($_POST['LatCoord']) || is_numeric($_POST['LatCoord'])) && 
+	(is_float($_POST['LongCoord']) || is_numeric($_POST['LongCoord'])) && 
+	(!is_null($_POST['Location']))) {
+		$dbQuery = sprintf("SELECT ID FROM Location WHERE Location.Name = '%s' AND
+		Location.LatCoord = '%s' AND Location.LongCoord = '%s'",
+		mysql_real_escape_string($_POST['Location']),
+		mysql_real_escape_string($_POST['LatCoord']),
+		mysql_real_escape_string($_POST['LongCoord']));
+		$LocationCall = 1;
+	} elseif ((is_float($_POST['LatCoord']) || is_numeric($_POST['LatCoord'])) && 
+	(is_float($_POST['LongCoord']) || is_numeric($_POST['LongCoord']))) {
+		$dbQuery = sprintf("SELECT ID FROM Location WHERE Location.Name is null AND
+		Location.LatCoord = '%s' AND Location.LongCoord = '%s'",
+		mysql_real_escape_string($_POST['LatCoord']),
+		mysql_real_escape_string($_POST['LongCoord']));
+		$LocationCall = 2;
+	} elseif (!is_null($_POST['Location'])) {
+		$dbQuery = sprintf("SELECT ID FROM Location WHERE Location.Name = '%s' AND
+		Location.LatCoord is null AND Location.LongCoord is null",
+		mysql_real_escape_string($_POST['Location']));
+		$LocationCall = 3;
+	}
+	
+	s_echo("Location search SQL: ".$dbQuery);
+	
+	$result = getDBResultNoHarm($dbQuery);
+	
+	s_echo("Location result: ".count($result));
+	
+	if (count($result) > 1) {
+		$LocationID = $result[0]['ID'];
+	} elseif (count($result) == 1) {
+		$LocationID = $result['ID'];
+	} else {
+		// need to insert to the Location Table
+		switch ($LocationCall){
+			case 1: $dbQuery = sprintf("INSERT INTO Location(LatCoord,LongCoord,Name)
+			VALUES ('%s','%s','%s')",mysql_real_escape_string($_POST['LatCoord']),
+			mysql_real_escape_string($_POST['LongCoord']),
+			mysql_real_escape_string($_POST['Location'])); break;
+			
+			case 2: $dbQuery = sprintf("INSERT INTO Location(LatCoord,LongCoord,Name)
+			VALUES ('%s','%s',NULL)",mysql_real_escape_string($_POST['LatCoord']),
+			mysql_real_escape_string($_POST['LongCoord'])); break;
+			
+			case 3: $dbQuery = sprintf("INSERT INTO Location(LatCoord,LongCoord,Name)
+			VALUES (NULL,NULL,'%s')",mysql_real_escape_string($_POST['Location'])); break;
+			
+			case 4: $dbQuery = sprintf("INSERT INTO Location(LatCoord,LongCoord,Name)
+			VALUES (NULL,NULL,NULL)"); break;
+		}
+		
+		s_echo("Location insert SQL: ".$dbQuery);
+		
+		$result = getDBResultInserted($dbQuery,'ID');
+		
+		s_echo("Location insert SQL complete");
+		
+		$LocationID = $result['ID'];
+
+	}
+	
+	s_echo("Location ID: ". $LocationID);
+	
+	// EventType
+	
+	// Need to Search if that EventType exists
+	if (is_null($_POST['EventTypeDesc']))
+		$dbQuery = sprintf("SELECT ID FROM EventType 
+		WHERE EventType.EventTypeDesc is null");
+	else $dbQuery = sprintf("SELECT ID FROM EventType
+		WHERE EventType.EventTypeDesc = '%s'",
+		mysql_real_escape_string($_POST['EventTypeDesc']));
+	
+	s_echo("EventType search SQL: ".$dbQuery);
+	
+	$result = getDBResultNoHarm($dbQuery);
+	
+	s_echo("EventType result: ".count($result));
+	
+	if (count($result) > 1) {
+		// something wrong with the DB, but we will just pick up the first entry
+		$EventTypeID = $result[0]['ID'];
+	} elseif (count($result) == 1) {
+		$EventTypeID = $result['ID'];
+	} else {
+		// need to insert to the EventType Table
+		if (is_null($_POST['EventTypeDesc'])) {
+			$dbQuery = sprintf("INSERT INTO EventType(EventTypeDesc) VALUES (null)");
+		} else {
+			$dbQuery = sprintf("INSERT INTO EventType(EventTypeDesc) VALUES ('%s')",
+			mysql_real_escape_string($_POST['EventTypeDesc']));
+		}
+		
+		s_echo("EventType insert SQL: ".$dbQuery);
+		
+		$result = getDBResultInserted($dbQuery,'ID');
+		
+		s_echo("EventType insert success");
+		
+		$EventTypeID = $result['ID'];
+	}
+	
+	s_echo("EventType ID: ".$EventTypeID);
+	
+	// Finally, we can update the Event Table
+	// Parameters: Title, Description, StartTime, EndTime, LocationID, CreatorID, EventTypeID, ID
+	if (is_null($_POST['Description'])) {
+		$dbQuery = sprintf("UPDATE `Event` Set 
+		Title = '%s', Description = NULL, StartTime = '%s' , EndTime = '%s',
+		LocationID = '%s', CreatorID = '%s', EventTypeID = '%s', LastModified = '%s'
+		WHERE `Event`.ID  = '%s'",
+		mysql_real_escape_string($_POST['Title']),
+		mysql_real_escape_string($_POST['StartTime']),
+		mysql_real_escape_string($_POST['EndTime']),
+		mysql_real_escape_string($LocationID),
+		mysql_real_escape_string($CreatorID),
+		mysql_real_escape_string($EventTypeID),
+		mysql_real_escape_string(new DateTime()),
+		mysql_real_escape_string($event_id)
+		);
+		
+	} else {
+
+		$dbQuery = sprintf("UPDATE `Event` Set 
+		Title = '%s', Description = '%s', StartTime = '%s' , EndTime = '%s',
+		LocationID = '%s', CreatorID = '%s', EventTypeID = '%s', LastModified = '%s'
+		WHERE `Event`.ID  = '%s'",
+		mysql_real_escape_string($_POST['Title']),
+		mysql_real_escape_string($_POST['Description']),
+		mysql_real_escape_string($_POST['StartTime']),
+		mysql_real_escape_string($_POST['EndTime']),
+		mysql_real_escape_string($LocationID),
+		mysql_real_escape_string($CreatorID),
+		mysql_real_escape_string($EventTypeID),
+		mysql_real_escape_string(new DateTime()),
+		mysql_real_escape_string($event_id)
+		);
+	}
+	
+	s_echo("Finally, Event SQL: ".$dbQuery);
+	
+    $result = getDBResultAffected($dbQuery,'EventID');
+    
+    s_echo("Event update success");
+    header("Content-type: application/json");
+    echo json_encode($result);
+	
 }
  
 function deleteEvent($id) {
+	
+	// verify the permission
+	
+	if (!eventPermissionRight($id)) {
+		$GLOBALS["_PLATFORM"]->sandboxHeader("HTTP/1.1 404 Not Found");
+		header("Content-type: application/json");
+        echo json_encode(array('error_msg'=> 'You do not have the permission.'));
+		die();
+	}
+	
 	if (!is_numeric($id)) { $id = 0; }
         $dbQuery = sprintf("DELETE FROM Event WHERE id = '%s' AND StartTime >= CURRENT_TIMESTAMP",
                 mysql_real_escape_string($id));                                                                                         
@@ -398,16 +684,23 @@ function listEventRSVP($event_id = 0) {
 	global $_USER;
 	$acctName = $_USER['uid'];
 	
-	// only Creator can get this, so check permission
+	// only that organization can get this, so check permission
 	
-	$dbQuery = sprintf("SELECT `AuthUser`.*, e.ID as `Event_ID` FROM `AuthUser`
-	JOIN `Organization` o ON `AuthUser`.OnBehalf = o.ID
-	JOIN `CreatorOwn` c ON `AuthUser`.ID = c.AuthUserID
-	JOIN `Event` e on c.CreatorID = e.CreatorID
-	WHERE `AuthUser`.AcctName = '%s' AND e.ID = '%s'",
-	mysql_real_escape_string($acctName), mysql_real_escape_string($event_id));
+	if (!eventPermissionRight($event_id)) {
+		$GLOBALS["_PLATFORM"]->sandboxHeader("HTTP/1.1 404 Not Found");
+		header("Content-type: application/json");
+        echo json_encode(array('error_msg'=> 'You do not have the permission.'));
+		die();
+	}
 	
-	$permission = getDBResultRecord($dbQuery); // the server will terminate if no permission
+	// $dbQuery = sprintf("SELECT `AuthUser`.*, e.ID as `Event_ID` FROM `AuthUser`
+	// JOIN `Organization` o ON `AuthUser`.OnBehalf = o.ID
+	// JOIN `CreatorOwn` c ON `AuthUser`.ID = c.AuthUserID
+	// JOIN `Event` e on c.CreatorID = e.CreatorID
+	// WHERE `AuthUser`.AcctName = '%s' AND e.ID = '%s'",
+	// mysql_real_escape_string($acctName), mysql_real_escape_string($event_id));
+	// 
+	// $permission = getDBResultRecord($dbQuery); // the server will terminate if no permission
 	
 	// it passes the permission test
 	
