dd34f318cf5708a7969f6353f79e8b999f35c5f2
google.johnh8888 <google.johnh8888@gmail.com>
2012-10-06 16:31:54 -0400

    capitalized yes

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -17,7 +17,7 @@ $(function() {
 			dataType: "json",
 			async: false,
 			success: function (data, textStatus, jqXHR) {
-				if (data.admin == "yes" && $('#manage_events_button').length == 0) {
+				if (data.admin == "Yes" && $('#manage_events_button').length == 0) {
 					$('#mainpages').append('<a href="#manage_events_page" id="manage_events_button" data-role="button" data-transition="slide" data-icon="arrow-r" data-iconpos="right" data-theme="d" style="display: none;">Manage Your Events</a>').trigger('create');
 				} else {
 					$('#manage_events_button').remove();
