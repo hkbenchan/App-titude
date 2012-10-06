4a56a64a07983dfa29aa531f3160dcd5d786bf0a
google.johnh8888 <google.johnh8888@gmail.com>
2012-10-06 17:00:27 -0400

    fix

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -17,8 +17,10 @@ $(function() {
 			dataType: "json",
 			async: false,
 			success: function (data, textStatus, jqXHR) {
-				if (data.admin == "Yes" && $('#manage_events_button').length == 0) {
-					$('#main_pages').append('<a href="#manage_events_page" id="manage_events_button" data-role="button" data-transition="slide" data-icon="arrow-r" data-iconpos="right" data-theme="d" style="display: none;">Manage Your Events</a>').trigger('create');
+				if (data.admin == "Yes") {
+					if ($('#manage_events_button').length == 0) {
+						$('#main_pages').append('<a href="#manage_events_page" id="manage_events_button" data-role="button" data-transition="slide" data-icon="arrow-r" data-iconpos="right" data-theme="d" style="display: none;">Manage Your Events</a>').trigger('create');
+					}
 				} else {
 					$('#manage_events_button').remove();
 				}
