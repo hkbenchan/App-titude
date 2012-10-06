3cb778b91377a68e21b6e78fe14b9ae74db573f5
google.johnh8888 <google.johnh8888@gmail.com>
2012-10-06 15:54:54 -0400

    added calendar page

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -7,6 +7,10 @@ $(function() {
  // Handler for .ready() called.
 	console.log('ready');
 	
+	$('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+	
 	$('#home_page').bind('pagebeforeshow',function(event, ui) {
 		$.ajax({
 			url: 'api/event/0/admin',
