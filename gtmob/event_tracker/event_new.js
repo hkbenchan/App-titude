c5883656f254f27750da2cc2cca41d6bfbd912f5
google.johnh8888 <google.johnh8888@gmail.com>
2012-10-06 14:14:33 -0400

    test

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -7,9 +7,9 @@ $(function() {
  // Handler for .ready() called.
 	console.log('ready');
 	
-	$('#calendar').fullCalendar({
+	/*$('#calendar').fullCalendar({
         // put your options and callbacks here
-    })
+    })*/
 	
 	$('#home_page').bind('pagebeforeshow',function(event, ui) {
 		$.ajax({
