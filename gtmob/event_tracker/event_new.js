0294010934d84c7d2e98b5d6211f9ee0feedad8b
google.johnh8888 <google.johnh8888@gmail.com>
2012-10-06 17:30:36 -0400

    test

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -19,14 +19,10 @@ $(function() {
 			success: function (data, textStatus, jqXHR) {
 				if (data.admin == "Yes") {
 					$('#manage_events_button').show();
+				} else {
+					$('#manage_events_button').hide();
 				}
 				$('#main_pages').controlgroup('refresh');
-			},
-			statusCode: {
-				401: function() {
-				  $('#manage_events_button').hide();
-				  $('#main_pages').controlgroup('refresh');
-				}
 			},
 			error: ajaxError
 		});
