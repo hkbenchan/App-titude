28c66c3bf1916f22278c1d153b0617707d6ad029
Eduardo Perez <edrdo.prz@gmail.com>
2012-10-07 16:21:53 -0400

    admin stuff

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -304,7 +304,7 @@ $(function() {
 				{
 				// add the AcctName for that
 				// 
-					$('#hold_manage_events').append(data[i]+'</br></br>');
+					$('#hold_manage_events').append(data[i].AcctName + '</br></br>');
 				}
 			},
 			error: function(data) {
