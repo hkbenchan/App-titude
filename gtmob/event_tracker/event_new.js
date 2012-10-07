461232bf65c13a28a0084f3d52d52527c1eaeed1
hoyin29 <hoyin29@gmail.com>
2012-10-07 15:26:01 -0300

    Update gtmob/event_tracker/event_new.js

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -446,58 +446,56 @@ $(function() {
 		});
 	});
 	
-	
-	/*
+
 	//******************** editing event post *************************
 	//need a button to bind to
-	$('#').bind('click',function()
+	$('#editid').bind('click',function()
 	{
 		console.log("editing an event post (id: " + event_ID + ")");
 		$.ajax
 		({
 			url: "api/event/"+event_ID,
 			dataType: "json",
-	        async: false,
+	       	 	async: false,
 			type: 'GET',
-	        success: function(data) 
-	        {
-	            
-				$('#enameid').val(data['Title']);
-				$('#eorganizationid').val(data['OrganizationName']);
-				$('#eemailid').val(data['Email_address']);
-				$('#ephoneid').val(data['Phone_number']);
-				$('#econtactid').val(data['Contact']);
-				$('#elocationid').val(data['Location']);
-				$('#etypeid').val(data['EventTypeDesc']);
-				$('#edescriptionid').val(data['Description']);
-				
-				var stemp = data['StartTime'].split(" ");
-				$('#estartdateid').val(stemp[0]);
-				$('#estarttimeid').val(stemp[1]);
-				
-			    var etemp = data['EndTime'].split(" ");
-		       	$('#eenddateid').val(etemp[0]);
-		       	$('#eendtimeid').val(etemp[1]);
-		       	
-		       	console.log("JSON object received successfully: ");
-		   
-		       	console.log("org: " + data['OrganizationName']);
-		       	console.log("title: " + data['Title']);
-		       	console.log("email: " + data['Email_address']);
-		       	console.log("phone: " + data['Phone_number']);
-		       	console.log("contact: " + data['Contact']);
-		       	console.log("location: " + data['LocationName']);
-		       	console.log("type: " + data['EventTypeDesc']);
-		       	console.log("description: " + data['Description']);
-		       	console.log("start date: " + stemp[0]);
-		       	console.log("start time: " + stemp[1]);
-		       	console.log("end date: " + etemp[0]);
-		       	console.log("end time: " + etemp[1]);
-		    },
-	        error: ajaxError
+		        success: function(data) 
+		        {
+		            
+					$('#enameid').val(data['Title']);
+					$('#eorganizationid').val(data['OrganizationName']);
+					$('#eemailid').val(data['Email_address']);
+					$('#ephoneid').val(data['Phone_number']);
+					$('#econtactid').val(data['Contact']);
+					$('#elocationid').val(data['Location']);
+					$('#etypeid').val(data['EventTypeDesc']);
+					$('#edescriptionid').val(data['Description']);
+					
+					var stemp = data['StartTime'].split(" ");
+					$('#estartdateid').val(stemp[0]);
+					$('#estarttimeid').val(stemp[1]);
+					
+				    var etemp = data['EndTime'].split(" ");
+			       	$('#eenddateid').val(etemp[0]);
+			       	$('#eendtimeid').val(etemp[1]);
+			       	
+			       	console.log("JSON object received successfully: ");
+			   
+			       	console.log("org: " + data['OrganizationName']);
+			       	console.log("title: " + data['Title']);
+			       	console.log("email: " + data['Email_address']);
+			       	console.log("phone: " + data['Phone_number']);
+			       	console.log("contact: " + data['Contact']);
+			       	console.log("location: " + data['LocationName']);
+			       	console.log("type: " + data['EventTypeDesc']);
+			       	console.log("description: " + data['Description']);
+			       	console.log("start date: " + stemp[0]);
+			       	console.log("start time: " + stemp[1]);
+			       	console.log("end date: " + etemp[0]);
+			       	console.log("end time: " + etemp[1]);
+			    },
+		        error: ajaxError
 		});
 	});
-	*/
 });
 
 function formatList(ele_div,data) {
