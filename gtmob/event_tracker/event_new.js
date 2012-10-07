ca4a9f801519e7864d32d5b1d28b3770e0fac63e
hoyin29 <hoyin29@gmail.com>
2012-10-07 19:37:14 -0300

    Update gtmob/event_tracker/event_new.js

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -532,6 +532,7 @@ $(function() {
 					success: function(data) 
 					{
 						console.log("event added successful");
+						window.location.replace("http://m.cip.gatech.edu/d/apptitude/w/event_tracker/content/");
 					},
 					error: ajaxError
 				});
@@ -563,6 +564,7 @@ $(function() {
 					success: function(data) 
 					{
 						console.log("event updated successful");
+						window.location.replace("http://m.cip.gatech.edu/d/apptitude/w/event_tracker/content/");
 					},
 					error: ajaxError
 				});
