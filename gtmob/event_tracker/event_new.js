7abee6390473ac5a5bffe641443813ea6470d40f
Eduardo Perez <edrdo.prz@gmail.com>
2012-10-07 13:53:35 -0400

    bug

--- gtmob/event_tracker/event_new.js
+++ gtmob/event_tracker/event_new.js
@@ -283,11 +283,11 @@ $(function() {
 					$.each(data[i],function(key,val) {
 						//$('#manage_list' + i).append('<li><a href="#view_event_page" data-event="' + val.ID + '" data-transition="slide"><h3>' + val.Title + '</h3></a></li>');
 						console.log(val);
-						$('#manage_list' + i).append('<li><div data-role="collapsible" class="manage_buttons_collapsible" id="manage_buttons_collapsible' + i + '" data-theme="c" data-content-theme="c" data-collapsed="false"><h3>' + val.Title + '</h3></div>');
+						$('#manage_list' + i).append('<li><div data-role="collapsible" class="manage_buttons_collapsible" id="manage_buttons_collapsible' + i + '" data-theme="b" data-content-theme="c" data-collapsed="false"><h3>' + val.Title + '</h3></div>');
 						//$('#event_list' + i).append('<li><a href="#view_event_page&event_id=' + val.ID + '" data-transition="slide"><h3>' + val.Title + '</h3></a></li>');
 						console.log(val.ID);
 						
-						$('#manage_buttons_collapsible' + i).append('<ul data-role="listview" class="manage_buttons_list" id="manage_buttons_list' + i + '" data-inset="true" data-theme="c">');
+						$('#manage_buttons_collapsible' + i).append('<ul data-role="listview" class="manage_buttons_list" id="manage_buttons_list' + i + '" data-inset="true" data-theme="d">');
 						$('#manage_buttons_list'+i).append('<li><a href="#view_event_page" data-event="' + val.ID + '" data-transition="slide"><h3>Edit</h3></a></li>');
 						$('#manage_buttons_list'+i).append('<li><a href="#view_event_page" data-event="' + val.ID + '" data-transition="slide"><h3>View RSVPs</h3></a></li>');
 					});
